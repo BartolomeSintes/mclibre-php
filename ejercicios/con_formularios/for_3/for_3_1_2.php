2b1fc7679dd24c03eaaf15c0bafb548572e9c475
BartolomeSintes <bartolome.sintes@gmail.com>
2017-04-11 00:02:24 +0200

    Versión inicial

---
+++ ejercicios/con_formularios/for_3/for_3_1_2.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Tabla de una fila (Resultado) - for_3_1_2.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2016 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2016-11-06
+ * @link      http://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <title>Tabla de una fila (Resultado). for (3).
+      Ejercicios. PHP. Bartolomé Sintes Marco</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="mclibre_php_soluciones.css" rel="stylesheet" type="text/css" title="Color" />
+  </head>
+
+  <body>
+    <h1>Tabla de una fila (Resultado)</h1>
+
+<?php
+function recoge($var)
+{
+    $tmp = (isset($_REQUEST[$var]))
+        ? trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"))
+        : "";
+    return $tmp;
+}
+
+$columnas = recoge("columnas");
+
+$columnasOk = false;
+
+$columnasMinimo = 1;
+$columnasMaximo = 200;
+
+if ($columnas == "") {
+    print "    <p class=\"aviso\">No ha escrito el número de columnas.</p>\n\n";
+} elseif (!is_numeric($columnas)) {
+    print "    <p class=\"aviso\">No ha escrito el número de columnas como número.</p>\n\n";
+} elseif (!ctype_digit($columnas)) {
+    print "    <p class=\"aviso\">No ha escrito el número de columnas "
+        . "como número entero positivo.</p>\n\n";
+} elseif ($columnas < $columnasMinimo || $columnas > $columnasMaximo) {
+    print "    <p class=\"aviso\">El número de columnas debe estar entre "
+        . "$columnasMinimo y $columnasMaximo.</p>\n\n";
+} else {
+    $columnasOk = true;
+}
+
+if ($columnasOk) {
+    print "    <table class=\"conborde\">\n";
+    print "      <tbody>\n";
+    print "        <tr>\n";
+    for ($i = 1; $i <= $columnas; $i++) {
+        print "          <td>$i</td>\n";
+    }
+    print "        </tr>\n";
+    print "      </tbody>\n";
+    print "    </table>\n";
+    print "\n";
+}
+
+?>
+    <p><a href="for_3_1_1.php">Volver al formulario.</a></p>
+
+    <footer>
+      <p class="ultmod">
+        Última modificación de esta página:
+        <time datetime="2016-11-06">6 de noviembre de 2016</time></p>
+
+      <p class="licencia">
+        Este programa forma parte del curso <a href="http://www.mclibre.org/consultar/php/">
+        Programación web en PHP</a> por <a href="http://www.mclibre.org/">Bartolomé
+        Sintes Marco</a>.<br />
+        El programa PHP que genera esta página está bajo
+        <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.</p>
+    </footer>
+  </body>
+</html>
