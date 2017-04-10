77e9ac3d6580b855f0a0b7cd89d36f99665bbfca
BartolomeSintes <bartolome.sintes@gmail.com>
2017-04-11 00:16:44 +0200

    Versión inicial

---
+++ ejercicios/sesiones/tragaperras/tragaperras_2_2.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Minijuegos: Tragaperras (2) - tragaperras_2_2.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2016 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2016-11-21
+ * @link      http://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+session_name("tragaperras_2");
+session_start();
+
+// Funciones auxiliares
+function recoge($var)
+{
+    $tmp = (isset($_REQUEST[$var]))
+    ? trim(htmlspecialchars($_REQUEST[$var], ENT_QUOTES, "UTF-8"))
+    : "";
+    return $tmp;
+}
+
+// Valores iniciales variables sesión
+if (!isset($_SESSION["monedas"])) {
+    $_SESSION["monedas"] = 0;
+}
+
+// Recogida de datos
+$accion  = recoge("accion");
+
+// Si se ha insertado moneda, se aumenta la cantidad de monedas
+if ($accion == "moneda") {
+    $_SESSION["monedas"] += 1;
+}
+
+// Redirección automática
+header("location:tragaperras_2_1.php");
---
+++ ejercicios/sin_formularios/for_1/for_1_21.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * for (1) 21 - for_1_21.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2016 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2016-10-04
+ * @link      http://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8" />
+    <title>Contar dados. for (1)
+      Ejercicios. Programación web en PHP. Bartolomé Sintes Marco</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="mclibre_php_soluciones.css" rel="stylesheet" type="text/css" title="Color" />
+  </head>
+
+  <body>
+    <h1>Contar dados</h1>
+
+    <p>Actualice la página para mostrar una nueva tirada.</p>
+
+<?php
+$numero = rand(1,10);
+$maximo = 0;
+$minimo = 7;
+$total = 0;
+
+print "    <h2>$numero dados</h2>\n";
+print "\n";
+print "    <p>\n";
+for ($i = 0; $i < $numero; $i++) {
+    $dado = rand(1, 6);
+    print "      <img src=\"img/$dado.svg\" alt=\"Dado 1\" title=\"$dado\" width=\"140\" height=\"140\">\n";
+    $total += $dado;
+    if ($dado > $maximo) {
+        $maximo = $dado;
+    }
+    if ($dado < $minimo) {
+        $minimo = $dado;
+    }
+}
+print "    </p>\n";
+print "\n";
+print "    <h2>Estadísticas</h2>\n";
+print "\n";
+print "    <ul>\n";
+print "      <li>El valor más grande obtenido ha sido <strong>$maximo</strong>.</li>\n";
+print "      <li>El valor más pequeño obtenido ha sido <strong>$minimo</strong>.</li>\n";
+print "      <li>El total de puntos obtenido ha sido <strong>$total</strong>.</li>\n";
+print "    </ul>\n";
+?>
+
+    <footer>
+      <p class="ultmod">
+        Última modificación de esta página:
+        <time datetime="2016-10-04">4 de octubre de 2016</time></p>
+
+      <p class="licencia">
+        Este programa forma parte del curso <a href="http://www.mclibre.org/consultar/php/">
+        Programación web en PHP</a> por <a href="http://www.mclibre.org/">Bartolomé
+        Sintes Marco</a>.<br />
+        El programa PHP que genera esta página está bajo
+        <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.</p>
+    </footer>
+  </body>
+</html>
