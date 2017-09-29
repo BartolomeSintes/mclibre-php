c158c43df3f9fafb43c14fdacaabeb47002e07a8
BartolomeSintes <bartolome.sintes@gmail.com>
2017-09-29 06:47:39 +0200

    Añado ejercicio Sin formularios > Variables 1

--- ejercicios/sin_formularios/variables_1/variables-1-13.php
+++ ejercicios/sin_formularios/variables_1/variables-1-13.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Variables (1) 12 - variables-1-12.php
+ * Variables (1) 13 - variables-1-13.php
  *
  * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
  * @copyright 2017 Bartolomé Sintes Marco
  * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
- * @version   2017-09-27
+ * @version   2017-09-29
  * @link      http://www.mclibre.org
  *
 *  This program is free software: you can redistribute it and/or modify
@@ -26,29 +26,27 @@
 <html lang="es">
 <head>
   <meta charset="utf-8" />
-  <title>Tres cuadrados. Variables.
+  <title>Tres círculos. Variables.
     Ejercicios. Programación web en PHP. Bartolomé Sintes Marco</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="mclibre-php-soluciones.css" rel="stylesheet" type="text/css" title="Color" />
 </head>
 
 <body>
-  <h1>Tres cuadrados</h1>
+  <h1>Tres círculos</h1>
 
-  <p>Actualice la página para mostrar tres nuevos cuadrados.</p>
+  <p>Actualice la página para mostrar tres nuevos círculos.</p>
 
 <?php
 $c1 = rand(50, 150);
 $c2 = rand(50, 150);
 $c3 = rand(50, 150);
-$ancho = $c1 + $c2 + $c3 + 20;
-$alto = max($c1, $c2, $c3) + 20;
 
 print "  <p><svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\"\n";
-print "    width=\"$ancho\" height=\"$alto\" viewBox=\"-10 -10 $ancho $alto\" style=\"background-color: white;\" font-family=\"sans-serif\">\n";
-print "    <rect x=\"0\" y=\"0\" width=\"$c1\" height=\"$c1\" fill=\"red\" />\n";
-print "    <rect x=\"$c1\" y=\"0\" width=\"$c2\" height=\"$c2\" fill=\"green\" />\n";
-print "    <rect x=\"" . ($c1+$c2) . "\" y=\"0\" width=\"$c3\" height=\"$c3\" fill=\"blue\" />\n";
+print "    width=\"920\" height=\"320\" viewBox=\"-10 -10 920 320\" style=\"background-color: white;\" font-family=\"sans-serif\">\n";
+print "    <circle cx=\"$c1\" cy=\"150\" r=\"$c1\" stroke=\"black\" stroke-width=\"2\" fill=\"red\" />\n";
+print "    <circle cx=\"" . (2 * $c1 + $c2) . "\" cy=\"150\" r=\"$c2\" stroke=\"black\" stroke-width=\"2\" fill=\"green\" />\n";
+print "    <circle cx=\"" . (2 * $c1 + 2 * $c2 + $c3) ."\" cy=\"150\" r=\"$c3\" stroke=\"black\" stroke-width=\"2\" fill=\"blue\" />\n";
 print "  </svg></p>\n";
 ?>
 
