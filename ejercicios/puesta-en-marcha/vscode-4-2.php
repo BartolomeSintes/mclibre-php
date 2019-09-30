d784517f08d27a91cd91d55cee4f3e8a05d8e771
BartolomeSintes <bartolome.sintes@gmail.com>
2019-09-30 23:55:03 +0200

    Ejercicios VSCode Puesta en marcha: Añado ejercicio para comprobar configuración output_buffering

---
+++ ejercicios/puesta-en-marcha/vscode-4-2.php
@@ -0,0 +1,10 @@
+﻿<?php
+header("location:vscode-4-3.php");
+?>
+<p>Esta es la página 2.</p>
+
+<p>Si ve esta página es que tiene la configuración recomendada para desarrollo recomendada en estos apuntes.</p>
+
+<p>Debe ver un aviso <strong>Warning</strong> al principio de esta página.</p>
+
+<p><a href="vscode-4-1.php">Volver al principio</a></p>
---
+++ ejercicios/puesta-en-marcha/vscode-4-3.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8" />
+  <title>BOM y redirecciones</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+  <p>Esta es la página 3.</p>
+
+  <p>Si ve esta página es que <strong>NO</strong> tiene la configuración recomendada para desarrollo recomendada en estos apuntes.</p>
+  
+  <p>Por favor, revise la directiva output_buffering en php.ini, como se explica en la lección 
+    <a href="http://www.mclibre.org/consultar/php/otros/php-configuracion-1.html#output-buffering">Configuración de Apache y PHP</a>.</p>
+
+  <p><a href="vscode-4-1.php">Volver al principio</a></p>
+</body>
+</html>
