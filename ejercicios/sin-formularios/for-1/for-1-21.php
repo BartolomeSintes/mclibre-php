926b20defef45759d1c78f04ebdc6a57d58a4e62
BartolomeSintes <bartolome.sintes@gmail.com>
2021-10-19 11:13:40 +0200

    Ejercicios SF > For (1): añado ayuda en ejercicio 21 (diana) y modifico solución

--- ejercicios/sin-formularios/for-1/for-1-21.php
+++ ejercicios/sin-formularios/for-1/for-1-21.php
@@ -5,7 +5,7 @@
  * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
  * @copyright 2021 Bartolomé Sintes Marco
  * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
- * @version   2021-10-12
+ * @version   2021-10-19
  * @link      https://www.mclibre.org
  *
  *  This program is free software: you can redistribute it and/or modify
@@ -55,9 +55,9 @@ $disparos = rand(1, 10);
 for ($i = 0; $i < $disparos; $i++) {
     $x = rand(-200, 200);
     $y = rand(-200, 200);
-    $puntos += ((10 - floor(sqrt($x ** 2 + $y ** 2) / 20)) > 0 ? (10 - floor(sqrt($x ** 2 + $y ** 2) / 20)) : 0);
+    $puntos += ((10 - floor(sqrt(pow($x, 2) + pow($y, 2)) / 20)) > 0 ? (10 - floor(sqrt(pow($x, 2) + pow($y, 2)) / 20)) : 0);
     print "      <path fill=\"black\" stroke=\"white\" stroke-width=\"2\" "
-     . "d=\"m $x,$y m 0,-9 4.4,2.5 3.6,0.6 0.5,2.9 2.2,2.9 -2.2,3.1 "
+     . "d=\"m $x,$y m -1,-9 4.4,2.5 3.6,0.6 0.5,2.9 2.2,2.9 -2.2,3.1 "
      . "-0.1,3.6 -3.3,0.2 -1.7,2.7 -4,-1.4 -3.9,0.2 -0.9,-4.2 -2.7,-2.6 1.7,-3.4 0,-3 z\" />\n";
 }
 print "    </svg>\n";
