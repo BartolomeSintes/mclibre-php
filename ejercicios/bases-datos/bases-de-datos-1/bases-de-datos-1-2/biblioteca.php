47a90bed62e53181a67bffbf8ed7fd5e8147abc9
BartolomeSintes <bartolome.sintes@gmail.com>
2022-01-08 00:47:09 +0100

    Ejercicios Bases de Datos: unifico hojas de estilo

--- ejercicios/bases-datos/bases-de-datos-1/bases-de-datos-1-2/biblioteca.php
+++ ejercicios/bases-datos/bases-de-datos-1/bases-de-datos-1-2/biblioteca.php
@@ -92,7 +92,7 @@ function pie()
     print "  <footer>\n";
     print "    <p class=\"ultmod\">\n";
     print "      Última modificación de esta página:\n";
-    print "      <time datetime=\"2021-12-30\">30 de diciembre de 2021</time>\n";
+    print "      <time datetime=\"2022-01-07\">7 de enero de 2022</time>\n";
     print "    </p>\n";
     print "\n";
     print "    <p class=\"licencia\">\n";
