9e73503b108d2026d04b63737a36bb2a5b263447
BartolomeSintes <bartolome.sintes@gmail.com>
2022-02-04 00:51:35 +0100

    Ejercicios Bases de datos 3(B): añado solución ejercicio 3

---
+++ ejercicios/bases-datos/bases-de-datos-3-b/bases-de-datos-3-b-3/comunes/biblioteca-postgresql.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * @author    Bartolomé Sintes Marco - bartolome.sintes+mclibre@gmail.com
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt AGPL 3 or later
+ * @link      https://www.mclibre.org
+ */
+
+// FUNCIONES ESPECÍFICAS DE LA BASE DE DATOS pgsql
+
+// POSTGRESQL: Nombres de las tablas
+
+$cfg["dbPersonasTabla"] = "personas";   // Nombre de la tabla Personas
+$cfg["dbUsuariosTabla"] = "usuarios";   // Nombre de la tabla Usuarios
+
+// POSTGRESQL: Conexión con la base de datos
+
+function conectaDb()
+{
+    global $cfg;
+
+    try {
+        $conexion = "$cfg[pgsqlHost];port=$cfg[pgsqlPort];dbname=$cfg[pgsqlDatabase];user=$cfg[pgsqlUser];password=$cfg[pgsqlPassword]";
+        $tmp      = new PDO($conexion);
+        $tmp->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+        return $tmp;
+    } catch (PDOException $e) {
+        print "    <p class=\"aviso\">Error: No puede conectarse con la base de datos. {$e->getMessage()}</p>\n";
+        exit;
+    }
+}
+
+// POSTGRESQL: Borrado y creación de tablas
+
+function borraTodo()
+{
+    global $pdo, $cfg;
+
+    print "    <p>Sistema Gestor de Bases de Datos: PostgreSQL.</p>\n";
+    print "\n";
+
+    $consulta = "DROP TABLE IF EXISTS $cfg[dbUsuariosTabla]";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error al borrar la tabla Usuarios. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Tabla Usuarios borrada correctamente (si existía).</p>\n";
+    }
+    print "\n";
+
+    $consulta = "DROP TABLE IF EXISTS $cfg[dbPersonasTabla]";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error al borrar la tabla Personas. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Tabla Personas borrada correctamente (si existía).</p>\n";
+    }
+    print "\n";
+
+    $consulta = "CREATE TABLE $cfg[dbUsuariosTabla]  (
+                 id SERIAL PRIMARY KEY,
+                 usuario VARCHAR($cfg[dbUsuariosTamUsuario]),
+                 password VARCHAR($cfg[dbUsuariosTamPassword]),
+                 nivel INTEGER NOT NULL
+                 )";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error al crear la tabla Usuarios. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Tabla usuarios creada correctamente.</p>\n";
+        print "\n";
+
+        $consulta = "INSERT INTO $cfg[dbUsuariosTabla]
+                     (id, usuario, password, nivel)
+                     VALUES (1, '$cfg[rootName]', '$cfg[rootPassword]', " . NIVEL_ADMINISTRADOR . ")";
+
+        if (!$pdo->query($consulta)) {
+            print "    <p class=\"aviso\">Error al insertar el registro de usuario. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+        } else {
+            print "    <p>Registro de usuario creado correctamente.</p>\n";
+        }
+    }
+    print "\n";
+
+    $consulta = "CREATE TABLE $cfg[dbPersonasTabla]  (
+                 id SERIAL PRIMARY KEY,
+                 nombre VARCHAR($cfg[dbPersonasTamNombre]),
+                 apellidos VARCHAR($cfg[dbPersonasTamApellidos]),
+                 telefono VARCHAR($cfg[dbPersonasTamTelefono]),
+                 correo VARCHAR($cfg[dbPersonasTamCorreo])
+                 )";
+
+    if (!$pdo->query($consulta)) {
+        print "    <p class=\"aviso\">Error al crear la tabla Personas. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } else {
+        print "    <p>Tabla creada correctamente.</p>\n";
+    }
+    print "\n";
+}
+
+// POSTGRESQL: Comprobación de existencia de las tablas
+
+function existenTablas()
+{
+    global $pdo, $cfg;
+
+    $existe = true;
+
+    foreach ($cfg["dbTablas"] as $tabla) {
+        $consulta = "SELECT COUNT(*) FROM information_schema.tables WHERE table_name='$tabla'";
+
+        $resultado = $pdo->query($consulta);
+        if (!$resultado) {
+            $existe = false;
+            print "    <p class=\"aviso\">Error en la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+            print "\n";
+        } else {
+            if ($resultado->fetchColumn() == 0) {
+                $existe = false;
+            }
+        }
+    }
+    return $existe;
+}
---
+++ ejercicios/bases-datos/bases-de-datos-3-b/bases-de-datos-3-b-3/db/tabla-personas/modificar-2.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * @author    Bartolomé Sintes Marco - bartolome.sintes+mclibre@gmail.com
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt AGPL 3 or later
+ * @link      https://www.mclibre.org
+ */
+
+require_once "../../comunes/biblioteca.php";
+
+session_name($cfg["sessionName"]);
+session_start();
+
+if (!isset($_SESSION["conectado"]) || $_SESSION["conectado"] < NIVEL_USUARIO_BASICO) {
+    header("Location:../../index.php");
+    exit;
+}
+
+$pdo = conectaDb();
+
+cabecera("Personas - Modificar 2", MENU_PERSONAS, PROFUNDIDAD_2);
+
+$id = recoge("id");
+
+if ($id == "") {
+    print "    <p class=\"aviso\">No se ha seleccionado ningún registro.</p>\n";
+} else {
+    $consulta = "SELECT * FROM $cfg[dbPersonasTabla]
+                 WHERE id = :id";
+
+    $resultado = $pdo->prepare($consulta);
+    if (!$resultado) {
+        print "    <p class=\"aviso\">Error al preparar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } elseif (!$resultado->execute([":id" => $id])) {
+        print "    <p class=\"aviso\">Error al ejecutar la consulta. SQLSTATE[{$pdo->errorCode()}]: {$pdo->errorInfo()[2]}</p>\n";
+    } elseif (!($registro = $resultado->fetch())) {
+        print "    <p class=\"aviso\">Registro no encontrado.</p>\n";
+    } else {
+        print "    <form action=\"modificar-3.php\" method=\"$cfg[formMethod]\">\n";
+        print "      <p>Modifique los campos que desee:</p>\n";
+        print "\n";
+        print "      <table>\n";
+        print "        <tbody>\n";
+        print "          <tr>\n";
+        print "            <td>Nombre:</td>\n";
+        print "            <td><input type=\"text\" name=\"nombre\" size=\"$cfg[dbPersonasTamNombre]\" maxlength=\"$cfg[dbPersonasTamNombre]\" value=\"$registro[nombre]\" autofocus></td>\n";
+        print "          </tr>\n";
+        print "          <tr>\n";
+        print "            <td>Apellidos:</td>\n";
+        print "            <td><input type=\"text\" name=\"apellidos\" size=\"$cfg[dbPersonasTamApellidos]\" maxlength=\"$cfg[dbPersonasTamApellidos]\" value=\"$registro[apellidos]\"></td>\n";
+        print "          </tr>\n";
+        print "          <tr>\n";
+        print "            <td>Teléfono:</td>\n";
+        print "            <td><input type=\"text\" name=\"telefono\" size=\"$cfg[dbPersonasTamTelefono]\" maxlength=\"$cfg[dbPersonasTamTelefono]\" value=\"$registro[telefono]\"></td>\n";
+        print "          </tr>\n";
+        print "          <tr>\n";
+        print "            <td>Correo:</td>\n";
+        print "            <td><input type=\"text\" name=\"correo\" size=\"$cfg[dbPersonasTamCorreo]\" maxlength=\"$cfg[dbPersonasTamCorreo]\" value=\"$registro[correo]\"></td>\n";
+        print "          </tr>\n";
+        print "        </tbody>\n";
+        print "      </table>\n";
+        print "\n";
+        print "      <p>\n";
+        print "        <input type=\"hidden\" name=\"id\" value=\"$id\">\n";
+        print "        <input type=\"submit\" value=\"Actualizar\">\n";
+        print "        <input type=\"reset\" value=\"Reiniciar formulario\">\n";
+        print "      </p>\n";
+        print "    </form>\n";
+    }
+}
+
+$pdo = null;
+
+pie();
