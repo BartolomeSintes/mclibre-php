5327f2f0dd82bc9cb8ef9dcd55e20c54fbb18e41
BartolomeSintes <bartolome.sintes@gmail.com>
2022-06-09 22:57:21 +0200

    Ejercicios Con Formularios > Controles (2): Corrijo error en ej. 11

--- ejercicios/con-formularios/controles-formularios-2/controles-formularios-2-11-2.php
+++ ejercicios/con-formularios/controles-formularios-2/controles-formularios-2-11-2.php
@@ -72,9 +72,6 @@ if ($lado == "") {
 } elseif ($lado < 20 || $lado > 500) {
     print "  <p class=\"aviso\">El tamaño del lado no está entre 20 y 500 px.</p>\n";
     print "\n";
-} elseif ($lado < $esquina * 2) {
-    print "  <p class=\"aviso\">El tamaño del lado debe ser al menos el doble que el de la esquina.</p>\n";
-    print "\n";
 } else {
     $ladoOk = true;
 }
@@ -96,6 +93,15 @@ if ($esquina == "") {
 }
 
 if ($ladoOk && $esquinaOk) {
+    if ($lado < $esquina * 2) {
+        print "  <p class=\"aviso\">El tamaño del lado debe ser al menos el doble que el de la esquina.</p>\n";
+        print "\n";
+        $ladoOk    = false;
+        $esquinaOk = false;
+    }
+}
+
+if ($ladoOk && $esquinaOk) {
     print "  <svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" \n"
         . "    width=\"" . ($lado + 10) . "px\" height=\"" . ($lado + 10) . "px\">\n";
     print "    <rect fill=\"white\" stroke=\"black\" stroke-width=\"10\" "
@@ -109,7 +115,7 @@ if ($ladoOk && $esquinaOk) {
   <footer>
     <p class="ultmod">
       Última modificación de esta página:
-      <time datetime="2019-10-24">24 de octubre de 2019</time>
+      <time datetime="202-06-09">9 de junio de 2022</time>
     </p>
 
     <p class="licencia">
