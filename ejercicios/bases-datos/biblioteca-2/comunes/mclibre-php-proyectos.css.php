53862880173a74a811d8a58a150e489151199979
BartolomeSintes <bartolome.sintes@gmail.com>
2022-09-27 13:52:19 +0200

    CSS proyectos: cambio colores a hwb()

--- ejercicios/bases-datos/biblioteca-2/comunes/mclibre-php-proyectos.css.php
+++ ejercicios/bases-datos/biblioteca-2/comunes/mclibre-php-proyectos.css.php
@@ -4,135 +4,107 @@ header("Content-type: text/css");
 ?>
 /* PROGRAMACION WEB EN PHP                  */
 /* Bartolome Sintes Marco                   */
-/* https://www.mclibre.org                   */
+/* https://www.mclibre.org                  */
 /*                                          */
 /* CSS ejercicios proyectos                 */
 /*                                          */
-/* 7 de diciembre de 2017                   */
+/* 27 de septiembre de 2022                 */
 /*                                          */
 
 /* Esta parte es comun a todos los ejercicios proyectos */
 
-html, body {
-  margin: 0;
-  padding: 0;
-  background-color: hsl(<?= COLOR ?>, 80%, 95%);
-  color: black;
-  font-family: sans-serif;
+html,
+body {
+    margin: 0;
+    padding: 0;
+    background-color: hwb(<?= COLOR ?>, 90%, 5%);
+    color: black;
+    font-family: sans-serif;
 }
 
 h1 {
-  margin: 0;
-  padding: 5px 20px;
-  background-color: hsl(<?= COLOR ?>, 80%, 60%);
-  color: white;
-  text-transform: uppercase;
+    margin: 0;
+    padding: 5px 20px;
+    background-color: hwb(<?= COLOR ?>, 30%, 5%);
+    color: white;
+    text-transform: uppercase;
 }
 
 nav {
-  margin: 0;
-  background-color: hsl(<?= COLOR ?>, 80%, 75%);
-  color: white;
+    margin: 0;
+    background-color: hwb(<?= COLOR ?>, 50%, 5%);
+    color: white;
 }
 
 nav ul {
-  margin: 0;
-  padding: 5px;
-  list-style-type: none;
+    margin: 0;
+    padding: 5px;
+    list-style-type: none;
 }
 
 nav li {
-  display: inline;
-  padding: 0 15px;
+    display: inline;
+    padding: 0 15px;
 }
 
 nav a {
-  background-color: hsl(<?= COLOR ?>, 80%, 75%);
-  color: white;
-  font-weight: bold;
+    background-color: hwb(<?= COLOR ?>, 50%, 5%);
+    color: white;
+    font-weight: bold;
 }
 
 main {
-  display: block; /* lo necesita IE9 IE10 IE11 */
-  padding: 10px 20px;
+    padding: 10px 20px;
 }
 
 input {
-  font-family: monospace;
+    font-family: monospace;
 }
 
 .boton-invisible {
-  border: none;
-  padding: 0;
-  background-color: transparent;
+    padding: 0;
+    border: none;
+    background-color: transparent;
 }
 
 .aviso {
-  color: red;
+    color: red;
 }
 
 .centrado {
-  text-align: center;
+    text-align: center;
 }
 
 a img {
-  border: none;
+    border: none;
 }
 
-table.conborde, table.conborde td, table.conborde th {
-  border: black 1px solid;
+table.conborde,
+table.conborde td,
+table.conborde th {
+    border: black 1px solid;
 }
 
-thead tr, table.franjas tbody tr:nth-child(even) {
-  background-color: hsl(<?= COLOR ?>, 80%, 85%);
-  color: black;
+thead tr,
+table.franjas tbody tr:nth-child(even) {
+    background-color: hwb(<?= COLOR ?>, 70%, 5%);
+    color: black;
 }
 
-td, th {
-  padding-left: 5px;
-  padding-right: 5px;
+td,
+th {
+    padding-right: 5px;
+    padding-left: 5px;
 }
 
 footer {
-  clear: both;
-  border-top: black solid 1px;
-  margin: 30px 20px 0;
+    clear: both;
+    margin: 30px 20px 0;
+    border-top: black solid 1px;
 }
 
 footer cite {
-  font-weight: bold;
+    font-weight: bold;
 }
 
-/* Esta parte es especifica de este ejercicio */
-
-table.calendario {
-}
-
-table.calendario caption {
-  margin-left: auto;
-  margin-right: auto;
-  padding-bottom: 3px;
-}
-
-table.calendario th {
-  background-color: hsl(<?= COLOR ?>, 80%, 85%);
-  color: black;
-  font-weight: bold;
-}
-
-table.calendario td {
-  text-align: center;
-  padding: 5px 10px;
-}
-
-table.calendario td.enlace {
-  background-color: hsl(<?= COLOR ?>, 80%, 85%);
-  color: blue;
-}
-
-table.calendario a {
-  color: blue;
-  font-weight: bold;
-  text-decoration: none;
-}
-
+/* Esta parte es especifica de este proyecto */
