fe239c1bd7bff84611cc761fdef98daab8089369
BartolomeSintes <bartolome.sintes@gmail.com>
2022-09-28 23:37:53 +0200

    Operaciones: aritméticas: actualizo lección y amplio algunos apartados

---
+++ lecciones/ejemplos/operaciones-aritmeticas-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Valores máximos tipos numéricos. Operaciones aritméticas.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+print "<p>PHP_INT_MAX = " . PHP_INT_MAX . "</p>\n";
+print "\n";
+print "<p>PHP_INT_MAX - 1 = " . PHP_INT_MAX - 1 . "</p>\n";
+print "\n";
+print "<p>PHP_INT_MAX + 1 = " . PHP_INT_MAX + 1 . "</p>\n";
+?>
+</body>
+</html>
---
+++ lecciones/ejemplos/operaciones-aritmeticas-2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Valores máximos tipos numéricos. Operaciones aritméticas.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+print "<p>PHP_FLOAT_MAX = " . PHP_FLOAT_MAX . "</p>\n";
+print "\n";
+print "<p>PHP_FLOAT_MAX - 1 = " . PHP_FLOAT_MAX - 1E292 . "</p>\n";
+print "\n";
+print "<p>PHP_FLOAT_MAX + 1 = " . PHP_FLOAT_MAX + 1E292 . "</p>\n";
+?>
+</body>
+</html>
