b96e6fc9b379047a5550a1cca6aad84ac3528212
BartolomeSintes <bartolome.sintes@gmail.com>
2022-11-05 00:18:03 +0100

    Comprobación de datos: añado ejemplo de 2 controles

---
+++ lecciones/ejemplos/formularios/form-recogida-input-checkbox-1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Recogida input checkbox. Recogida de datos.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <form action="form-recogida-input-checkbox-2.php" method="get">
+    <p>
+        ¿Sabe programar en estos lenguajes?
+        <input type="checkbox" name="python" value="Sí"> Python
+        <input type="checkbox" name="php" value="Sí"> PHP
+    </p>
+
+    <p><input type="submit" value="Enviar"></p>
+  </form>
+</body>
+</html>
---
+++ lecciones/ejemplos/formularios/form-recogida-input-checkbox-2.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Recogida input checkbox. Recogida de datos.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style> .aviso { color: red; }</style>
+</head>
+
+<body>
+<?php
+// Función de recogida de datos
+function recoge($var, $m = "")
+{
+    $tmp = is_array($m) ? [] : "";
+    if (isset($_REQUEST[$var])) {
+        if (!is_array($_REQUEST[$var]) && !is_array($m)) {
+            $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+        } elseif (is_array($_REQUEST[$var]) && is_array($m)) {
+            $tmp = $_REQUEST[$var];
+            array_walk_recursive($tmp, function (&$valor) {
+                $valor = trim(htmlspecialchars($valor));
+            });
+        }
+    }
+    return $tmp;
+}
+
+// Variables que recogen los datos
+$python = recoge("python");
+$php    = recoge("php");
+
+// Variables auxiliares de comprobación
+$pythonOk = false;
+$phpOk    = false;
+
+// Validación de datos y generación de avisos
+if ($python != "Sí" && $python != "") {
+    print "  <p class=\"aviso\">Por favor, indique si sabe programar o no en Python.</p>\n";
+    print "\n";
+} else {
+    $pythonOk = true;
+}
+
+if ($php != "Sí" && $php != "") {
+    print "  <p class=\"aviso\">Por favor, indique si sabe programar o no en PHP.</p>\n";
+    print "\n";
+} else {
+    $phpOk = true;
+}
+
+// Si todo es correcto, ejecución del programa
+if ($pythonOk && $phpOk) {
+    if ($python && $php) {
+        print "  <p>Sabe programar en Python y en PHP.</p>\n";
+        print "\n";
+    } elseif ($python && !$php) {
+        print "  <p>Sabe programar en Python, pero no en PHP.</p>\n";
+        print "\n";
+    } elseif (!$python && $php) {
+        print "  <p>Sabe programar en PHP, pero no en Python.</p>\n";
+        print "\n";
+    } else {
+        print "  <p>No sabe programar ni en Python ni en PHP.</p>\n";
+        print "\n";
+    }
+}
+?>
+
+<p><a href="form-recogida-input-checkbox-1.php">Volver al formulario.</a></p>
+</body>
+</html>
