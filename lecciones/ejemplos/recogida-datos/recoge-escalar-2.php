1fb7c94e9256afbfb9434d0941ad5142d665a82c
BartolomeSintes <bartolome.sintes@gmail.com>
2022-11-08 23:07:10 +0100

    Recogida de datos: Añado ejemplos función recoge()

---
+++ lecciones/ejemplos/recogida-datos/recoge-escalar-2.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Función recoge() (dato escalar). Recogida de datos.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style> .aviso { color: red; }</style>
+</head>
+
+<body>
+<?php
+// Función de recogida de datos
+function recoge($var, $m = "")
+{
+    $tmp = is_array($m) ? [] : "";
+    if (isset($_REQUEST[$var])) {
+        if (!is_array($_REQUEST[$var]) && !is_array($m)) {
+            $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+        } elseif (is_array($_REQUEST[$var]) && is_array($m)) {
+            $tmp = $_REQUEST[$var];
+            array_walk_recursive($tmp, function (&$valor) {
+                $valor = trim(htmlspecialchars($valor));
+            });
+        }
+    }
+    return $tmp;
+}
+
+// Variables que recogen los datos
+$nombre   = recoge("nombre");
+$apellidos = recoge("apellidos");
+
+if ($nombre == "") {
+    print "<p>No ha escrito ningún nombre</p>";
+} else {
+    print "<p>Su nombre es <strong>$nombre</strong>.</p>\n";
+}
+
+if ($apellidos == "") {
+    print "<p>No ha escrito ningún apellido</p>";
+} else {
+    print "<p>Sus apellidos son <strong>$apellidos</strong>.</p>\n";
+}
+?>
+
+<p><a href="recoge-escalar-1.php">Volver al formulario.</a></p>
+</body>
+</html>
---
+++ lecciones/ejemplos/recogida-datos/recoge-matriz-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Función recoge() (dato matriz). Recogida de datos.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <form action="recoge-matriz-2.php" method="get">
+    <p>Nombre: <input type="text" name="nombre[1]"></p>
+
+    <p>Apellidos: <input type="text" name="nombre[2]"></p>
+
+    <p><input type="submit" value="Enviar"></p>
+  </form>
+</body>
+</html>
