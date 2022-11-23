fe9e67f905154b41ecf545cac049306b2a8df3d7
BartolomeSintes <bartolome.sintes@gmail.com>
2022-11-23 23:37:04 +0100

    CS Ejercicios Sesiones (1): añado tooltips (el js está ahora en fichero separado)

---
+++ varios/mclibre.js
@@ -0,0 +1,18 @@
+function muestra() {
+    var elementos = document.querySelectorAll("[tooltip-id]");
+    for (var i = 0; i < elementos.length; i++) {
+        const x1 = elementos[i].getAttribute("tooltip-id");
+        if (document.getElementById('tp-' + x1)) {
+            document.getElementById('tp-' + x1).remove();
+        }
+        elementos[i].classList.add("tooltip");
+        const x2 = document.getElementById(x1).textContent;
+        const tp = document.createElement("span");
+        tp.classList.add("tooltiptext");
+        tp.setAttribute('id', 'tp-' + x1);
+        tp.textContent = x2;
+        elementos[i].appendChild(tp)
+        elementos[i].style.display = 'revert';
+    }
+}
+window.onload = muestra;
