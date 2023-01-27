f3b88f28e42483368434a5f505216d915f078216
BartolomeSintes <bartolome.sintes@gmail.com>
2023-01-28 00:28:33 +0100

    Ejercicios Bases de datos: corrijo detalles en varias páginas

--- ejercicios/bases-datos/bases-de-datos-3-c/db-cambia-password/db/tabla-usuarios/password-1.php
+++ ejercicios/bases-datos/bases-de-datos-3-c/db-cambia-password/db/tabla-usuarios/password-1.php
@@ -1,6 +1,8 @@
 <?php
 /**
- * @author Escriba aquí su nombre
+ * @author    Bartolomé Sintes Marco - bartolome.sintes+mclibre@gmail.com
+ * @license   https://www.gnu.org/licenses/agpl-3.0.txt AGPL 3 or later
+ * @link      https://www.mclibre.org
  */
 
 require_once "../../comunes/biblioteca.php";
