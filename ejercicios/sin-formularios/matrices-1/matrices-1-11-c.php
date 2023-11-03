f55a85cc85945b07541acb86e8b21fc8941b295f
BartolomeSintes <bartolome.sintes@gmail.com>
2023-11-03 18:41:21 +0100

    Ejercicios SF Matrices (1): añado variantes de soluciones en dos ejercicios

---
+++ ejercicios/sin-formularios/matrices-1/matrices-1-11-c.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Matrices (1) 11 - matrices-1-11-c.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2023 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2023-11-03
+ * @link      https://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Tirada de dados.
+    Matrices (1). Sin formularios.
+    Ejercicios. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Tirada de dados</h1>
+
+  <p>Actualice la página para mostrar una nueva tirada.</p>
+
+<?php
+$numero = rand(2, 7);
+
+print "  <h2>Tirada de $numero dados</h2>\n";
+print "\n";
+
+// Guardamos los valores de los dados en la matriz $dados y
+// mostramos las imágenes de los dados obtenidos
+print "  <p>\n";
+for ($i = 0; $i < $numero; $i++) {
+    $dados[] = rand(1, 6);
+    print "    <img src=\"img/$dados[$i].svg\" alt=\"$dados[$i]\" width=\"140\" height=\"140\">\n";
+}
+print "  </p>\n";
+print "\n";
+
+// Mostramos los valores numéricos de los dados obtenidos
+print "  <h2>Resultado</h2>\n";
+print "\n";
+print "  <p>Los valores obtenidos son: ";
+foreach ($dados as $dado) {
+    print "$dado ";
+}
+print "</p>\n";
+?>
+
+  <footer>
+    <p class="ultmod">
+      Última modificación de esta página:
+      <time datetime="2023-11-03">3 de noviembre de 2023</time>
+    </p>
+
+    <p class="licencia">
+      Este programa forma parte del curso <strong><a href="https://www.mclibre.org/consultar/php/">Programación
+      web en PHP</a></strong> de <a href="https://www.mclibre.org/" rel="author">Bartolomé Sintes Marco</a>.<br>
+      El programa PHP que genera esta página se distribuye bajo
+      <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.
+    </p>
+  </footer>
+</body>
+</html>
---
+++ ejercicios/sin-formularios/matrices-1/matrices-1-12-c.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Matrices (1) 12 - matrices-1-12-b.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2023 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2023-11-03
+ * @link      https://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Negación.
+    Matrices (1). Sin formularios.
+    Ejercicios. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Negación de bits</h1>
+
+  <p>Actualice la página para mostrar una secuencia aleatoria de bits y su complementaria.</p>
+
+<?php
+$numero = 10;
+
+// Creamos la matriz de bits aleatorios y la matriz con valores complementarios
+$inicial   = [];
+$resultado = [];
+for ($i = 0; $i < $numero; $i++) {
+    $inicial[$i]   = rand(0, 1);
+    $resultado[$i] = $inicial[$i] == 1 ? 0 : 1;
+}
+
+// Mostramos los bits aleatorios
+print "  <pre style=\"font-size: 300%;\">\n";
+print "A: ";
+foreach ($inicial as $bit) {
+    print "$bit ";
+}
+print "\n";
+print "\n";
+
+// Mostramos los valores complementarios
+print "<span style=\"text-decoration: overline\">A</span>: ";
+foreach ($resultado as $bit) {
+    print "$bit ";
+}
+print "\n";
+print "</pre>\n";
+?>
+
+  <footer>
+    <p class="ultmod">
+      Última modificación de esta página:
+      <time datetime="2023-11-03">3 de noviembre de 2023</time>
+    </p>
+
+    <p class="licencia">
+      Este programa forma parte del curso <strong><a href="https://www.mclibre.org/consultar/php/">Programación
+      web en PHP</a></strong> de <a href="https://www.mclibre.org/" rel="author">Bartolomé Sintes Marco</a>.<br>
+      El programa PHP que genera esta página se distribuye bajo
+      <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.
+    </p>
+  </footer>
+</body>
+</html>
