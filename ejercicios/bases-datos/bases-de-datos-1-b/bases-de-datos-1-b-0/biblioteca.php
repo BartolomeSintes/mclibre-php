6adefe217c00ad50f15ffbe855b4b32de07b74b3
BartolomeSintes <bartolome.sintes@gmail.com>
2024-01-15 22:33:15 +0100

    Ejercicios Bases de datos: cambio fecha de todos los ejercicios

--- ejercicios/bases-datos/bases-de-datos-1-b/bases-de-datos-1-b-0/biblioteca.php
+++ ejercicios/bases-datos/bases-de-datos-1-b/bases-de-datos-1-b-0/biblioteca.php
@@ -120,7 +120,7 @@ function pie()
     print "  <footer>\n";
     print "    <p class=\"ultmod\">\n";
     print "      Última modificación de esta página:\n";
-    print "      <time datetime=\"2024-01-14\">14 de enero de 2024</time>\n";
+    print "      <time datetime=\"2024-01-15\">15 de enero de 2024</time>\n";
     print "    </p>\n";
     print "\n";
     print "    <p class=\"licencia\">\n";
