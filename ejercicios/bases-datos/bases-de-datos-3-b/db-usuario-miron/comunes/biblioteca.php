3daddb99d35bc7238c81971b6ee4e1cc04e21e5e
BartolomeSintes <bartolome.sintes@gmail.com>
2024-01-24 17:47:33 +0100

    Ejercicios Bases de datos: actualizo fecha a 2024-01-24

--- ejercicios/bases-datos/bases-de-datos-3-b/db-usuario-miron/comunes/biblioteca.php
+++ ejercicios/bases-datos/bases-de-datos-3-b/db-usuario-miron/comunes/biblioteca.php
@@ -215,7 +215,7 @@ function pie()
     print "  <footer>\n";
     print "    <p class=\"ultmod\">\n";
     print "      Última modificación de esta página:\n";
-    print "      <time datetime=\"2024-01-23\">23 de enero de 2024</time>\n";
+    print "      <time datetime=\"2024-01-24\">24 de enero de 2024</time>\n";
     print "    </p>\n";
     print "\n";
     print "    <p class=\"licencia\">\n";
