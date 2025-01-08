96c35eec9d5c082a8ab0ae66cb2eb9bafa9b03b9
BartolomeSintes <bartolome.sintes@gmail.com>
2025-01-08 19:07:58 +0100

    Ejercicios Controles en formularios (1): corrijo detalle (cambio print de 2 líneas por 2 print)

--- ejercicios/con-formularios/controles-formularios-1/controles-formularios-1-2-2.php
+++ ejercicios/con-formularios/controles-formularios-1/controles-formularios-1-2-2.php
@@ -63,8 +63,8 @@ function recoge($key, $type = "")
 
 $ancho = recoge("ancho");
 
-print "  <svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" \n"
-    . "    width=\"{$ancho}px\" height=\"{$ancho}px\">\n";
+print "  <svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" \n";
+print "    width=\"{$ancho}px\" height=\"{$ancho}px\">\n";
 print "    <rect x=\"0\" y=\"0\" width=\"$ancho\" height=\"$ancho\" fill=\"black\" />\n";
 print "  </svg>\n";
 ?>
