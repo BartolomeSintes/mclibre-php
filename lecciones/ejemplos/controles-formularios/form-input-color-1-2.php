420ef6f299420e963dc2a7f65ecf81779baa44ea
BartolomeSintes <bartolome.sintes@gmail.com>
2026-01-08 14:31:39 +0100

    Controles en formularios: añado varios controles HTML5

---
+++ lecciones/ejemplos/controles-formularios/form-input-color-1-2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    input color (2). Controles en Formularios.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+<?php
+print "  <pre>\n";
+print_r($_REQUEST);
+print "<a href=\"form-input-color-2-1.php\">Volver al formulario.</a>\n";
+print "</pre>\n";
+?>
+</body>
+</html>
---
+++ lecciones/ejemplos/controles-formularios/form-input-time-1-1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    input time (1). Controles en Formularios.
+    Ejemplos. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <form action="form-input-time-1-2.php" method="get">
+    <p><input type="time" name="cajaHora1"></p>
+
+    <p><input type="submit" value="Enviar"></p>
+  </form>
+</body>
+</html>
