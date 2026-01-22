111ed16455cbfb7500d09505ae2af22fd62e96fc
BartolomeSintes <bartolome.sintes@gmail.com>
2026-01-22 09:28:35 +0100

    Ejercicios SF Variables (0): añado otra solución en ejercicio 2

---
+++ ejercicios/sin-formularios/variables-0/variables-0-02-c.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Sin formularios. Variables (0) 2 C - variables-0-02.php
+ *
+ * @author    Bartolomé Sintes Marco <bartolome.sintes+mclibre@gmail.com>
+ * @copyright 2025 Bartolomé Sintes Marco
+ * @license   http://www.gnu.org/licenses/agpl.txt AGPL 3 or later
+ * @version   2025-02-08
+ * @link      https://www.mclibre.org
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Media del Índice de Masa Corporal
+    Variables (0). Sin formularios.
+    Ejercicios. PHP. Bartolomé Sintes Marco. www.mclibre.org
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="mclibre-php-ejercicios.css" title="Color">
+</head>
+
+<body>
+  <h1>Cálculo del Índice de Masa Corporal</h1>
+
+  <p>Actualice la página para mostrar un nuevo cálculo.</p>
+
+<?php
+$peso   = rand(50, 100);
+$altura = rand(150, 200) / 100;
+$imc    = $peso / $altura ** 2;
+$imc    = round($imc, 1);
+
+print "  <p>Con un peso de $peso kg y una altura de $altura m, el IMC es $imc.</p>\n";
+print "\n";
+?>
+  <footer>
+    <p class="ultmod">
+      Última modificación de esta página:
+      <time datetime="2025-02-08">8 de febrero de 2025</time>
+    </p>
+
+    <p class="licencia">
+      Este programa forma parte del curso <strong><a href="https://www.mclibre.org/consultar/php/">Programación
+      web en PHP</a></strong> de <a href="https://www.mclibre.org/" rel="author">Bartolomé Sintes Marco</a>.<br>
+      El programa PHP que genera esta página se distribuye bajo
+      <a rel="license" href="http://www.gnu.org/licenses/agpl.txt">licencia AGPL 3 o posterior</a>.
+    </p>
+  </footer>
+</body>
+</html>
