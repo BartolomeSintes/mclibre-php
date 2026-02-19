c4056a97bbff92376262cd43c0e49cb597c23aa3
BartolomeSintes <bartolome.sintes@gmail.com>
2026-02-19 18:40:59 +0100

    Bases de datos 3 D: corrijo erratas en nombres de bases de datos

--- ejercicios/bases-datos/bases-de-datos-3-d/bases-de-datos-3-d-2/comunes/config.php
+++ ejercicios/bases-datos/bases-de-datos-3-d/bases-de-datos-3-d-2/comunes/config.php
@@ -22,9 +22,9 @@ $cfg["sqliteDatabase"] = "/tmp/mclibre-base-datos-3-d-2.sqlite";    // Ubicació
 // Configuración para MySQL
 
 $cfg["mysqlHost"]     = "localhost";                        // Nombre de host
-$cfg["mysqlUser"]     = "mclibre_base_datos_3_c_2";         // Nombre de usuario
+$cfg["mysqlUser"]     = "mclibre_base_datos_3_d_2";         // Nombre de usuario
 $cfg["mysqlPassword"] = "";                                 // Contraseña de usuario
-$cfg["mysqlDatabase"] = "mclibre_base_datos_3_c_2";         // Nombre de la base de datos
+$cfg["mysqlDatabase"] = "mclibre_base_datos_3_d_2";         // Nombre de la base de datos
 
 // Tamaño de los campos en la tabla Usuarios
 
--- ejercicios/bases-datos/bases-de-datos-3-d/bases-de-datos-3-d-4/comunes/config.php
+++ ejercicios/bases-datos/bases-de-datos-3-d/bases-de-datos-3-d-4/comunes/config.php
@@ -22,9 +22,9 @@ $cfg["sqliteDatabase"] = "/tmp/mclibre-base-datos-3-d-4.sqlite";    // Ubicació
 // Configuración para MySQL
 
 $cfg["mysqlHost"]     = "localhost";                        // Nombre de host
-$cfg["mysqlUser"]     = "mclibre_base_datos_3_c_4";         // Nombre de usuario
+$cfg["mysqlUser"]     = "mclibre_base_datos_3_d_4";         // Nombre de usuario
 $cfg["mysqlPassword"] = "";                                 // Contraseña de usuario
-$cfg["mysqlDatabase"] = "mclibre_base_datos_3_c_4";         // Nombre de la base de datos
+$cfg["mysqlDatabase"] = "mclibre_base_datos_3_d_4";         // Nombre de la base de datos
 
 // Número máximo de registros en las tablas (el valor 0 o negativo indica que no hay límite)
 
